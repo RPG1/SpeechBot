2a2d6b8395a4e473e134bf41f1c51b0fb72dd88b
sharon <rpgfxwq@gmail.com>
2023-01-28 09:36:25 +0200

    SpeechBot

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SBOT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdbox = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // cmdbox
+            // 
+            this.cmdbox.Font = new System.Drawing.Font("Sans Serif Collection", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdbox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.cmdbox.FormattingEnabled = true;
+            this.cmdbox.ItemHeight = 47;
+            this.cmdbox.Location = new System.Drawing.Point(552, 11);
+            this.cmdbox.Name = "cmdbox";
+            this.cmdbox.Size = new System.Drawing.Size(236, 427);
+            this.cmdbox.TabIndex = 0;
+            this.cmdbox.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 210);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(534, 29);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cmdbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox cmdbox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.IO;
+using System.Diagnostics;
+
+namespace SBOT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SpeechSynthesizer sp = new SpeechSynthesizer();
+
+        SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+
+        Grammar gramer = new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"cmd.txt"))));
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sp.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Teen);
+            rec.SetInputToDefaultAudioDevice();
+            rec.LoadGrammar(gramer);
+            rec.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(defualtspeech);
+            rec.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void defualtspeech(object sender, SpeechRecognizedEventArgs e)
+        {
+            string res = e.Result.Text;
+            if (res == "hello")
+            {
+                sp.SpeakAsync("Hi my name is Sbot your professional assistant");
+            }
+            if (res == "exit")
+            {
+                this.Close();
+            }
+            if (res == "show cmd")
+            {
+                string[] cmd = (File.ReadAllLines(@"cmd.txt"));
+
+                foreach (var item in cmd)
+                {
+                    cmdbox.Items.Add(item);
+                    cmdbox.Visible = true;
+                }
+            }
+            if (res == "hide cmd")
+            {
+                cmdbox.Items.Clear();
+                cmdbox.Visible = false;
+            }
+            if (res == "open youtube")
+            {
+                string youtube = "https://www.youtube.com/";
+                System.Diagnostics.Process.Start(youtube);
+            }
+            if (res == "go to my channel in youtube")
+            {
+                sp.SpeakAsync("Ok go to your channel");
+                string mychannel = "https://www.youtube.com/@mwcode8052/videos";
+                System.Diagnostics.Process.Start(mychannel);
+            }
+            if (res == "what time")
+            {
+                sp.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+        }
+    }
+}
